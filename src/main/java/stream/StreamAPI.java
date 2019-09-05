17ed5b39d4235f13ffd094a87e958ed20e15b540
张大宝 <1822373498@qq.com>
2019-09-05 11:13:33 +0800

    first commit

---
+++ src/main/java/stream/StreamAPI.java
@@ -0,0 +1,12 @@
+package stream;
+
+/**
+ * @author zhangnianlei
+ * @create 2019/9/5.
+ * @description stream应用
+ */
+public class StreamAPI {
+    public static void main(String[] args) {
+
+    }
+}
