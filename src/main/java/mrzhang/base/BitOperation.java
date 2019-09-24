21eed6ae4942fb71f34346b48945cda3c593cb1d
张念磊 <1822373498@qq.com>
2019-09-24 22:04:02 +0800

    生成随机数 位操作

---
+++ src/main/java/mrzhang/base/BitOperation.java
@@ -0,0 +1,13 @@
+package mrzhang.base;
+
+/**
+ * @author zhangnianlei
+ * @create 2019/9/24.
+ * @description 位运算
+ */
+public class BitOperation {
+    public static void main(String[] args) {
+        System.out.println(2^1);
+        System.out.println(2&1);
+    }
+}
--- src/main/java/mrzhang/base/RandomFunction.java
+++ src/main/java/mrzhang/base/RandomFunction.java
@@ -6,8 +6,10 @@ package mrzhang.base;
  * @description 随机生成30-60之间的随机数
  */
 public class RandomFunction {
+
     public static void main(String[] args) {
         Integer num = 30 + (int) (Math.random()*30);
         System.out.println(num);
     }
+
 }
