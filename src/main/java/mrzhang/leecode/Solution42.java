071d48fddab6bce6f73d1dc7bb9a39be08038819
张念磊 <1822373498@qq.com>
2020-01-05 01:09:39 +0800

    2020/1/5  42题-接雨水 看了三种解法 未编码

---
+++ src/main/java/mrzhang/leecode/Solution42.java
@@ -0,0 +1,28 @@
+package mrzhang.leecode;
+
+/**
+ * @author zhangnianlei
+ * @date 2020/1/5
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+ * 示例:
+ * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ * https://leetcode-cn.com/problems/trapping-rain-water/
+ *
+ * 三种解法：
+ * 暴力法 解决问题，简单粗暴
+ * 备忘录优化法 降低时间复杂度
+ * 双指针法 降低空间复杂度
+ */
+public class Solution42 {
+
+	static int trap(int[] height) {
+
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
+	}
+}
