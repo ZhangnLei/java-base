e6e7d5088e18adf5ebf332b441a3d5c6bfa60e83
张念磊 <1822373498@qq.com>
2020-01-08 14:37:47 +0800

    2020/1/8 46. 全排列 回溯算法没看懂

---
+++ src/main/java/mrzhang/leecode/Solution46.java
@@ -0,0 +1,21 @@
+package mrzhang.leecode;
+
+import java.util.List;
+
+/**
+ * @author zhangnianlei
+ * @date 2020/1/8
+ */
+public class Solution46 {
+	public static List<List<Integer>> permute(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3};
+		permute(nums);
+	}
+}
