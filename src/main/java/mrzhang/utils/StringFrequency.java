f6ef06f131dd1adc6b83e4904ec057dc6b3bf4da
张念磊 <1822373498@qq.com>
2020-01-18 09:37:41 +0800

    统计字符出现的频率

---
+++ src/main/java/mrzhang/utils/StringFrequency.java
@@ -0,0 +1,38 @@
+package mrzhang.utils;
+
+import java.util.*;
+
+/**
+ * @author zhangnianlei
+ * @date 2020/1/18
+ */
+public class StringFrequency {
+	public static void main(String[] args) {
+		String s = "fsorcocranb,ay,vlmhnee.wuiwto,”d.ulHOUPOhe”R,niysoFfo";
+		getFrequency(s);
+	}
+
+	private static void getFrequency(String s) {
+		Map<String, Integer> map = new HashMap<>(s.length());
+		for (String s1 : s.split("")) {
+			if (map.containsKey(s1)){
+				map.put(s1, map.get(s1) + 1);
+			}else {
+				map.put(s1, 1);
+			}
+		}
+		sortMap(map);
+	}
+
+	static void sortMap(Map<String, Integer> map){
+		List<Map.Entry<String, Integer>> listMap = new ArrayList<>();
+		listMap.addAll(map.entrySet());
+		Collections.sort(listMap, new Comparator<Map.Entry<String, Integer>>() {
+			@Override
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				return o2.getValue() - o1.getValue();
+			}
+		});
+		listMap.forEach(System.out::println);
+	}
+}
