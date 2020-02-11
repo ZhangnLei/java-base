b8e587704774468b1ba2323a9aaa15b083e3a50c
张念磊 <1822373498@qq.com>
2020-02-11 21:53:08 +0800

    添加

--- src/main/java/mrzhang/leecode/Solution4.java
+++ src/main/java/mrzhang/leecode/Solution4.java
@@ -1,7 +1,5 @@
 package mrzhang.leecode;
 
-import java.util.Arrays;
-
 /**
  * @author zhangnianlei
  * @date 2020/1/30
@@ -34,6 +32,7 @@ public class Solution4 {
 
 	/**
 	 * @description 暴力法
+	 * 时间复杂度：O(m+n)
 	 * 思路：
 	 * <p>将两个数组合并成一个数组，然后找出中位数</p>
 	 * <p>如何合并：</p>
@@ -50,7 +49,7 @@ public class Solution4 {
 	 * @return: double
 	 * @modifier
 	 */
-	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+	public static double findMedianSortedArraysVolice(int[] nums1, int[] nums2) {
 		int[] nums = mergeTwoArrays(nums1, nums2);
 		return getMedian(nums);
 	}
@@ -109,6 +108,24 @@ public class Solution4 {
 		return nums;
 	}
 
+	/**
+	 * @description 优化暴力法
+	 * 时间复杂度：O(m+n)/2 = O(m+n)
+	 * <p>优化思路：不需要将所有的数字都排好序，只需要找到index = lenght/2</p>
+	 * @author zhangnianlei
+	 * @date 2020/1/30
+	 * @exception
+	 * @param: nums1
+	 * @param: nums2
+	 * @return: double
+	 * @modifier
+	 */
+	public double findMedianSortedArrays1(int[] nums1, int[] nums2) {
+
+		return 1;
+	}
+
+
 	public static void main(String[] args) {
 //		int[] a = {1,2,3,4,5,6};
 //		int[] b = {0};
@@ -117,4 +134,37 @@ public class Solution4 {
 		double result = findMedianSortedArrays(a, b);
 		System.out.println(result);
 	}
+
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int n1 = nums1.length;
+		int n2 = nums2.length;
+		if (n1>n2)
+			return findMedianSortedArrays(nums2, nums1);
+		int k = (n1 + n2 + 1)/2;
+		int left = 0;
+		int right = n1;
+		while(left < right){
+			int m1 = left +(right - left)/2;
+			int m2 = k - m1;
+			if (nums1[m1] < nums2[m2-1])
+				left = m1 + 1;
+			else
+				right = m1;
+		}
+		int m1 = left;
+		int m2 = k - left;
+		int c1 = Math.max(m1 <= 0 ? Integer.MIN_VALUE : nums1[m1-1],
+				m2 <= 0 ? Integer.MIN_VALUE : nums2[m2-1]);
+		if ((n1 + n2) % 2 == 1) //奇数
+			return c1;
+		int c2 = Math.min( m1 >= n1 ? Integer.MAX_VALUE :nums1[m1],
+				m2 >= n2 ? Integer.MAX_VALUE : nums2[m2]);
+		return (c1 + c2) * 0.5;
+
+	}
+//
+//	作者：powcai
+//	链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/shuang-zhi-zhen-by-powcai/
+//	来源：力扣（LeetCode）
+//	著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 }
