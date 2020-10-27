c9aded7141145efd2655a6d05e988cc3bdc0b72a
张念磊 <1822373498@qq.com>
2020-10-27 11:16:54 +0800

    fix: 每个数据的第一条添加两次 且时间不同

--- src/main/java/mrzhang/sy/LocateDate.java
+++ src/main/java/mrzhang/sy/LocateDate.java
@@ -3,6 +3,8 @@ package mrzhang.sy;
 import org.apache.commons.lang.StringUtils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author zhangnianlei
@@ -11,11 +13,30 @@ import java.time.LocalDate;
 public class LocateDate {
 
 	public static void main(String[] args) {
-		System.out.println(getTime("2020-07-13 000.0"));
-		System.out.println(getTime("2020-07-13"));
-		System.out.println(getTime("2020-07-13"));
-		System.out.println(getTime("200.0"));
-		System.out.println(getTime("2020- 00.0"));
+//		System.out.println(getTime("2020-07-13 000.0"));
+//		System.out.println(getTime("2020-07-13"));
+//		System.out.println(getTime("2020-07-13"));
+//		System.out.println(getTime("200.0"));
+//		System.out.println(getTime("2020- 00.0"));
+//		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+////		System.out.println(LocalDateTime.now().toString());
+
+
+//		LocalDateTime time = LocalDateTime.now();
+//		String localTime = df.format(time);
+//		LocalDateTime ldt = LocalDateTime.parse("2017-09-28 17:07:05",df);
+//		ldt.
+//		System.out.println("LocalDateTime转成String类型的时间："+localTime);
+//		System.out.println("String类型的时间转成LocalDateTime："+ldt);
+
+
+
+
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime time = LocalDateTime.now();
+		String localTime = df.format(time);
+		System.out.println(localTime);
+
 	}
 
 
